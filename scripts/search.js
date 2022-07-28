dbe666be453715fa1abf6e55e730a440632eb6ea
Ayush Solanki <103575160+ayushsolanki29@users.noreply.github.com>
2022-07-28 17:54:05 +0530

    Add files via upload

---
+++ scripts/search.js
@@ -0,0 +1,6 @@
+const searchApps = () => {
+    let filter = document.getElementById('Search').value.toUpperCase();
+    let card = document.getElementByClassName('card');
+    let titel = card.getElementByClassName('text-title');
+    console.log('titel');
+}
