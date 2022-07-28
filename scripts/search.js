5bb73e06244d65e33bc3c77937ba7d9a0ffb24c1
Ayush Solanki <103575160+ayushsolanki29@users.noreply.github.com>
2022-07-28 17:51:08 +0530

    Delete scripts directory

--- scripts/search.js
+++
@@ -1,6 +0,0 @@
-const searchApps = () => {
-    let filter = document.getElementById('Search').value.toUpperCase();
-    let card = document.getElementByClassName('card');
-    let titel = card.getElementByClassName('text-title');
-    console.log('titel');
-}
